e65084f16420c0b0761e43bea068685d6fde78f0
mehmet <aydogan.18.34.18@gmail.com>
2025-07-08 18:01:47 +0300

    feat: PackageModel changed

--- src/models/PackageModel.py
+++ src/models/PackageModel.py
@@ -1,41 +1,7 @@
-
 from pydantic import Field, validator
 from typing import List, Optional, Union, Literal
-from sdks.novavision.src.base.model import Package, Image, Inputs, Configs, Outputs, Response, Request, Output, Input, Config
-
-
-class InputImage(Input):
-    name: Literal["inputImage"] = "inputImage"
-    value: Union[List[Image], Image]
-    type: str = "object"
-
-    @validator("type", pre=True, always=True)
-    def set_type_based_on_value(cls, value, values):
-        value = values.get('value')
-        if isinstance(value, Image):
-            return "object"
-        elif isinstance(value, list):
-            return "list"
-
-    class Config:
-        title = "Image"
-
-
-class OutputImage(Output):
-    name: Literal["outputImage"] = "outputImage"
-    value: Union[List[Image],Image]
-    type: str = "object"
-
-    @validator("type", pre=True, always=True)
-    def set_type_based_on_value(cls, value, values):
-        value = values.get('value')
-        if isinstance(value, Image):
-            return "object"
-        elif isinstance(value, list):
-            return "list"
-
-    class Config:
-        title = "Image"
+from sdks.novavision.src.base.model import Package, Image, Inputs, Configs, Outputs, Response, Request, Output, Input, \
+    Config
 
 
 class KeepSideFalse(Config):
@@ -76,7 +42,7 @@ class Degree(Config):
         Positive angles specify counterclockwise rotation while negative angles indicate clockwise rotation.
     """
     name: Literal["Degree"] = "Degree"
-    value: int = Field(ge=-359.0, le=359.0,default=0)
+    value: int = Field(ge=-359.0, le=359.0, default=0)
     type: Literal["number"] = "number"
     field: Literal["textInput"] = "textInput"
     placeHolder: Literal["[-359, 359]"] = "[-359, 359]"
@@ -85,22 +51,18 @@ class Degree(Config):
         title = "Angle"
 
 
-class PackageInputs(Inputs):
+class MScaleExecutorInputs(Inputs):
     inputImage: InputImage
 
 
-class PackageConfigs(Configs):
+class MScaleExecutorConfigs(Configs):
     degree: Degree
     drawBBox: KeepSideBBox
 
 
-class PackageOutputs(Outputs):
-    outputImage: OutputImage
-
-
-class PackageRequest(Request):
-    inputs: Optional[PackageInputs]
-    configs: PackageConfigs
+class MScaleExecutorRequest(Request):
+    inputs: Optional[MScaleExecutorInputs]  # opsiyonel
+    configs: MScaleExecutorConfigs
 
     class Config:
         json_schema_extra = {
@@ -108,13 +70,18 @@ class PackageRequest(Request):
         }
 
 
-class PackageResponse(Response):
-    outputs: PackageOutputs
+class MScaleExecutorOutputs(Outputs):
+    outputImage: OutputImage
 
 
-class PackageExecutor(Config):
-    name: Literal["Package"] = "Package"
-    value: Union[PackageRequest, PackageResponse]
+
+class MScaleExecutorResponse(Response):
+    outputs: MScaleExecuterOutputs
+
+
+class MScaleExecutor(Config):
+    name: Literal["MScaleExecutor"] = "MScaleExecutor"
+    value: Union[MScaleExecutorRequest, MScaleExecutorResponse]
     type: Literal["object"] = "object"
     field: Literal["option"] = "option"
 
@@ -129,14 +96,14 @@ class PackageExecutor(Config):
 
 class ConfigExecutor(Config):
     name: Literal["ConfigExecutor"] = "ConfigExecutor"
-    value: Union[PackageExecutor]
+    value: Union[MScaleExecutor]
     type: Literal["executor"] = "executor"
     field: Literal["dependentDropdownlist"] = "dependentDropdownlist"
 
     class Config:
         title = "Task"
         json_schema_extra = {
-            "target": "value"
+            "target": "value"  # tek executor varsa
         }
 
 
@@ -147,4 +114,8 @@ class PackageConfigs(Configs):
 class PackageModel(Package):
     configs: PackageConfigs
     type: Literal["component"] = "component"
-    name: Literal["Package"] = "Package"
+    name: Literal["M2Scaling"] = "Package"
+
+
+
+
