3a9296dc93e4ac3f826d3de93e97cad600dddb54
mehmet <aydogan.18.34.18@gmail.com>
2025-07-10 10:41:06 +0300

    executors edited

--- src/executors/HoughCircleExecutor.py
+++ src/executors/HoughCircleExecutor.py
@@ -11,7 +11,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
 from sdks.novavision.src.media.image import Image
 from sdks.novavision.src.base.component import Component
 from sdks.novavision.src.helper.executor import Executor
-from components.MScaling.src.utils.response import build_response
+from components.MScaling.src.utils.response import build_response_Circle
 from components.MScaling.src.models.PackageModel import PackageModel
 
 
