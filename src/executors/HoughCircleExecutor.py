eae643e10b3f90aa5aa15524d6580e6fdf8f154b
mehmet <aydogan.18.34.18@gmail.com>
2025-07-16 16:44:12 +0300

    HoughCircleExecutor edited

--- src/executors/HoughCircleExecutor.py
+++ src/executors/HoughCircleExecutor.py
@@ -65,10 +65,35 @@ class HoughCircleExecutor(Component):
 
         return img
 
+    def combine_images_side_by_side(self, img1, img2):
+        height = max(img1.shape[0], img2.shape[0])
+        width1 = img1.shape[1]
+        width2 = img2.shape[1]
+
+        # Yüksekliği eşitlemek için boşluk ekle
+        if img1.shape[0] < height:
+            img1 = cv2.copyMakeBorder(img1, 0, height - img1.shape[0], 0, 0, cv2.BORDER_CONSTANT, value=(0, 0, 0))
+        if img2.shape[0] < height:
+            img2 = cv2.copyMakeBorder(img2, 0, height - img2.shape[0], 0, 0, cv2.BORDER_CONSTANT, value=(0, 0, 0))
+
+        combined = cv2.hconcat([img1, img2])
+        return combined
+
     def run(self):
         img = Image.get_frame(img=self.image, redis_db=self.redis_db)
+        img2=Image.get_frame(img=self.image2, redis_db=self.redis_db)
+
         img.value = self.huffeman_circle_detection(img.value)
+
+        combined = self.combine_images_side_by_side(img.value, img2.value)
+
         self.image = Image.set_frame(img=img, package_uID=self.uID, redis_db=self.redis_db)
+        self.image2 = Image.set_frame(img=img2, package_uID=self.uID, redis_db=self.redis_db)
+
+        self.combinedImage = Image.set_frame_from_ndarray(img_array=combined
+                                                          ,package_uID=self.uID
+                                                          ,redis_db=self.redis_db)
+
         packageModel = build_response_Circle(context=self)
         return packageModel
 
