7bcb57b2ef0e10f385d80ee64377ec45b77142bc
mehmet <aydogan.18.34.18@gmail.com>
2025-07-17 22:16:32 +0300

    model example

--- src/executors/CatOrDog.py
+++ src/executors/CatOrDog.py
@@ -15,7 +15,7 @@ from sdks.novavision.src.base.component import Component
 from sdks.novavision.src.helper.executor import Executor
 from components.MScaling.src.utils.response import build_response_CatOrDog
 from components.MScaling.src.models.PackageModel import PackageModel
-#from components.MScaling.src.utils.utils import load_models
+from components.MScaling.src.utils.utils import load_models
 
 
 class CatOrDog(Component):
@@ -28,24 +28,15 @@ class CatOrDog(Component):
 
         #self.model = bootstrap["model"]
 
-    """
+
     @staticmethod
     def bootstrap(config: dict) -> dict:
         model = load_models(config=config)
         return {"model": model}
-"""
 
-    @staticmethod
-    def bootstrap(config: dict) -> dict:
-        return {}
-
-
-    #def predict_traffic_sign(img):
-     #   return img
 
     def run(self):
         img = Image.get_frame(img=self.image, redis_db=self.redis_db)
-        #img.value = self.predict_traffic_sign(img.value)
         self.image = Image.set_frame(img=img, package_uID=self.uID, redis_db=self.redis_db)
         packageModel = build_response_CatOrDog(context=self)
         return packageModel
