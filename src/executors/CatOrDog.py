d8547e9c0e51072909df7f85c288421146e7cbf2
mehmet <aydogan.18.34.18@gmail.com>
2025-07-17 22:11:17 +0300

    model example

--- src/executors/CatOrDog.py
+++ src/executors/CatOrDog.py
@@ -40,12 +40,12 @@ class CatOrDog(Component):
         return {}
 
 
-    def predict_traffic_sign(img):
-        return img
+    #def predict_traffic_sign(img):
+     #   return img
 
     def run(self):
         img = Image.get_frame(img=self.image, redis_db=self.redis_db)
-        img.value = self.predict_traffic_sign(img.value)
+        #img.value = self.predict_traffic_sign(img.value)
         self.image = Image.set_frame(img=img, package_uID=self.uID, redis_db=self.redis_db)
         packageModel = build_response_CatOrDog(context=self)
         return packageModel
